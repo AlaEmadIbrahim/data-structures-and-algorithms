503190739b6e89251de3b57ba27fc880b508c7aa
AlaEmadIbrahim <ALA.EMAD.IBRAHIM@GMAIL.COM>
2023-09-12 23:11:02 +0300

    done

--- codechallenge/codeChall37/graph-business-trip.js
+++ codechallenge/codeChall37/graph-business-trip.js
@@ -1,17 +1,26 @@
 "use strict";
 
-function buisnessTrip(graph, cities) {
+function businessTrip(graph, cities) {
   const departure = cities[0];
   const arrival = cities[1];
+
   const availableDestinations = graph.adjacencyList.get(departure);
 
-  if (!availableDestinations) return null;
+  if (!availableDestinations) {
+    return null;
+  }
+
   for (let i = 0; i < availableDestinations.length; i++) {
-    if (availableDestinations[i][0] === arrival) {
-      return availableDestinations[i][1];
+    const [destinationCity, cost] = availableDestinations[i];
+
+    // If the destination city matches the arrival city, return the cost
+    if (destinationCity === arrival) {
+      return cost;
     }
   }
+  // if not return null 
   return null;
 }
 
-module.exports = buisnessTrip;
+module.exports = businessTrip;
+
